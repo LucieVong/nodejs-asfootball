0765d1d0da3e30b1ebccffe75cac3f6c80391b49
LucieVong <35452145+LucieVong@users.noreply.github.com>
2018-05-19 16:07:25 +0200

    Delete client.sql

--- client.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `client` (`nom`, `email`, `pass`) VALUES
-('Dupont',	'dupontel@gmail.com',	'ddpm123'),
-('John',	'john@example.com',	'johnoe77'),
-('Laurine',	'lau-rine@gmail.com',	'lalappm'),
-('uhif',	'edihed@ffff.fr',	'dddddeeef');
